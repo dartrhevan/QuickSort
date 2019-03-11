407f6d6fb2b24a08bfa2052f789dd32a0b2a7916
dartrhevan <vovasatunkin@mail.ru>
2019-03-11 20:02:04 +0500

    First unworking algorith version

---
+++ QuickSort/HoareSort.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Threading;
+
+namespace QuickSort
+{
+    public class HoareSort
+    {
+        public static void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+
+        static void QuickSort(int[] array, int begin, int end)
+        {
+            if (begin == end) return;
+            var selectedElement = array[end];
+            var currentInd = begin;
+            for (var i = begin; i < end; ++i)
+            {
+                if (array[i] <= selectedElement)
+                    Swap(ref selectedElement, ref array[currentInd++]);
+            }
+            Swap(ref array[currentInd], ref array[end]);
+            if (currentInd > begin) QuickSort(array, begin, currentInd - 1);
+            if (currentInd < end) QuickSort(array, currentInd + 1, end);
+        }
+
+        static void Swap<T>(ref T element1, ref T element2)
+        {
+            var temp = element1;
+            element1 = element2;
+            element2 = temp;
+        }
+    }
+}
--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -10,6 +10,10 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
+            var arr = new[] {0, 5, -6, 63, 57, 93, 5, 2, 1, 7, 9, 4, 36, 4, 9};
+            HoareSort.QuickSort(arr);
+            foreach (var el in arr)
+                Console.WriteLine(el + " ");
         }
     }
 }
